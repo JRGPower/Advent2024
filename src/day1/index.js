174bedfff9138372f0c7464a87d5ebd4a80c3607
JRGPower <87418556+JRGPower@users.noreply.github.com>
2024-12-01 17:46:36 +0000

    intial ts setup

---
+++ src/day1/index.js
@@ -0,0 +1,10 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const fs_1 = __importDefault(require("fs"));
+const input = fs_1.default.readFileSync('src/day1/inputList.txt', 'utf-8');
+const splitcolumns = (str) => {
+    console.log(str);
+};
---
+++ src/day1/index.ts
@@ -0,0 +1,7 @@
+import fs from 'fs';
+
+const input = fs.readFileSync('src/day1/inputList.txt', 'utf-8');
+
+const splitcolumns = (str: string): string[] => {
+console.log(str)
+}
